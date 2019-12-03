4286aa8b3dd6e38d312930079d51260d6c2f5765
julio <38661081+juljoseg@users.noreply.github.com>
2019-12-02 23:15:15 -0500

    Add Unit Test

---
+++ src/test/java/com/example/demo/controller/UserControllerTest.java
@@ -0,0 +1,23 @@
+package com.example.demo.controller;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserControllerTest {
+	
+	@Autowired
+	private UserController userController;
+	
+	@Test
+	public void contextLoads() throws Exception {
+		assertThat(userController).isNotNull();
+	}
+
+}
