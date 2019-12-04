d966df218fc29ccaefb98d14b00243b550169734
julio <38661081+juljoseg@users.noreply.github.com>
2019-12-04 12:21:45 -0500

    Integrate with UI

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,15 +1,21 @@
 package com.example.demo.controller;
 
+
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Message;
+
 
 @RestController
 public class UserController {
 
+	
 	@RequestMapping("/message")
-	public String greeting(@RequestParam(value = "text") String text) {
-		return text;
+	public String greeting(@RequestBody Message message) {
+		System.out.println(" --------------> message : " + message == null ? "" : message.getText());
+		return message.getText();
 	}
-
+	
 }
